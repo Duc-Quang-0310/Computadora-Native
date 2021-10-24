165b74a0b019d6ce29751260923a8ec8739fed55
reactplus-hungnh <hungnh@reactplus.dev>
2021-10-24 11:28:09 +0700

    done profile UI

--- apps/reduxToolKit-Saga/Blogs/BlogSlice.ts
+++ apps/reduxToolKit-Saga/Blogs/BlogSlice.ts
@@ -28,7 +28,7 @@ export const BlogSlice = createSlice({
             state.progress = true
         },
         fetchingSuccess: (state, action: PayloadAction<iFetchBlogSuccess>) => {
-            console.log("action.payload.datas blog", action.payload.datas);
+            // console.log("action.payload.datas blog", action.payload.datas);
             state.datas = action.payload.datas
             state.success = action.payload.success
             state.fetchStatus = true
